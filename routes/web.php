fc5f2e0a0bf2c2eedefb3c9a75655c3957018027
Adriano Messias <asmessias@gmail.com>
2024-10-23 15:36:13 -0300

    Novo Projeto. Aulas da seção 13 completas. Aulas de 215 a 218 da seção 14. Todos realizados na Conab.

---
+++ routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Mail\MensagemTesteMail;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::resource('tarefa', 'App\Http\Controllers\TarefaController');
+Route::get('/mensagem-teste', function() {
+    //return new MensagemTesteMail();
+    Mail::to('adriano.messias@conab.gov.br')->send(new MensagemTesteMail());
+    return 'E-mail enviado com sucesso';
+});
